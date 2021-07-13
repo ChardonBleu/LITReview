747863532a5c69f1550d950904128808b5ca683f
Fabienne RONDI <f.rondi@orange.fr>
2021-07-13 16:59:05 +0200

    Docstrings admin.py

--- library/admin.py
+++ library/admin.py
@@ -6,6 +6,10 @@ from .forms import CustomUserCreationForm
 
 
 class CustomUserAdmin(UserAdmin):
+    """
+    customization of the UserAdmin class in order to make it call a
+    personalized form for the creation of a new user
+    """
     add_form = CustomUserCreationForm
 
 
