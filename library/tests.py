0fa18bffd60b6afa7e199b1fc63de83c26825aa2
Fabienne RONDI <f.rondi@orange.fr>
2021-07-16 09:55:49 +0200

    first test

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+from .models import User
+
 
 # Create your tests here.
+class UserViewTests(TestCase):
+    """[summary]
+
+    Arguments:
+        TestCase {[type]} -- [description]
+    """
+
+    def setUp(self):
+        """[summary]
+        """
+        self.current_user = User.objects.create(username='current_user_test', password='user_test')
+        self.other_user = User.objects.create(username="other_user_test", password="other_user")
+        self.client = Client()
+
+    def test_login(self):
+        """[summary]
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        print("page acceuil OK")
+        response = self.client.get(reverse('library:register'))
+        self.assertEqual(response.status_code, 200)
+        print("page inscription OK")
