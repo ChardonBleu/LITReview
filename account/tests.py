b60618c9bfe374cf1c40095bf4eb941b3942574f
Fabienne RONDI <f.rondi@orange.fr>
2021-07-28 12:22:00 +0200

    Test admin

--- account/tests.py
+++ account/tests.py
@@ -2,7 +2,6 @@ import pytest
 from django.urls import reverse
 from django.test import Client
 
-
 from .models import User
 
 
@@ -35,6 +34,16 @@ class TestUserView:
     def test_user_create(self):
         User.objects.create_user('toto', 'zeropluszero')
         assert User.objects.count() == 1
-    
+
+    @pytest.mark.django_db
+    def test_unauthorized(self):
+        response = self.client.get('/admin/')
+        assert response.status_code == 302
+
+    @pytest.mark.django_db
+    def test_superuser_view(self, admin_client):
+        response = admin_client.get('/admin/')
+        assert response.status_code == 200
+
     def test_registration(self):
         pass
