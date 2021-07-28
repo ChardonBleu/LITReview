56f4d81e37ee4929b9b6f71eb4cb1bf2761d1c92
Fabienne RONDI <f.rondi@orange.fr>
2021-07-28 13:27:04 +0200

    Test login et redirection vers flow après login

--- account/tests.py
+++ account/tests.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 from django.urls import reverse
 from django.test import Client
 
@@ -31,19 +32,45 @@ class TestUserView:
         assert response.status_code == 302
 
     @pytest.mark.django_db
-    def test_user_create(self):
-        User.objects.create_user('toto', 'zeropluszero')
-        assert User.objects.count() == 1
-
-    @pytest.mark.django_db
-    def test_unauthorized(self):
+    def test_unauthorized_user_on_admin(self):
         response = self.client.get('/admin/')
         assert response.status_code == 302
 
     @pytest.mark.django_db
-    def test_superuser_view(self, admin_client):
+    def test_superuser_on_admin(self, admin_client):
         response = admin_client.get('/admin/')
         assert response.status_code == 200
 
+    @pytest.fixture
+    def test_password(self):
+        return 'test-pass'
+
+    @pytest.fixture
+    def create_user(self, db, django_user_model, test_password):
+        def make_user(**kwargs):
+            kwargs['password'] = test_password
+            if 'username' not in kwargs:
+                kwargs['username'] = str(uuid.uuid4())
+            return django_user_model.objects.create_user(**kwargs)
+        return make_user
+
+    @pytest.fixture
+    def auto_login_user(self, db, client, create_user, test_password):
+        def make_auto_login(user=None):
+            if user is None:
+                user = create_user()
+            client.login(username=user.username, password=test_password)
+            return client, user
+        return make_auto_login
+
+    @pytest.mark.django_db
+    def test_auth_view(self, auto_login_user):
+        client, user = auto_login_user()
+        url = reverse('library:flow')
+        response = client.get(url)
+        assert response.status_code == 200
+
+    @pytest.mark.django_db
     def test_registration(self):
-        pass
+        User.objects.create_user('toto', 'zeropluszero')
+        assert User.objects.count() == 1
