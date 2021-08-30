ce883b11da7411576d1cc988cd77f9ace1a26e2f
Fabienne RONDI <f.rondi@orange.fr>
2021-08-30 22:46:38 +0200

    Filtration list of users to follow: exclude authenticated user

--- library/forms.py
+++ library/forms.py
@@ -1,6 +1,7 @@
+from account.models import User
 from django import forms
 from django.forms.widgets import RadioSelect
-from .models import Ticket, Review
+from .models import Ticket, Review, UserFollows
 
 
 class TicketForm(forms.ModelForm):
@@ -29,3 +30,18 @@ class ReviewForm(forms.ModelForm):
             'rating': RadioSelect(attrs={
                 'class': ' form-check form-check-inline'}),
         }
+
+class FollowedForm(forms.ModelForm):
+    """used to add followers subscriptions
+    exclusion of authenticated user un form list
+    
+    """
+    class Meta:
+        model = UserFollows
+        fields = ['followed_user']
+        
+    def __init__(self, *args, **kwargs):
+        username = kwargs.pop('user')
+        super(FollowedForm, self).__init__(*args, **kwargs)
+        self.fields['followed_user']= forms.ModelChoiceField(queryset=User.objects.all().exclude(username=username))
+    
